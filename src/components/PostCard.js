fb19ee0c6d44667c73aecfc10ba64c8794bccd7b
Davi Feliciano <dfeliciano37@gmail.com>
2023-06-04 18:38:58 -0300

    feat: like system

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,7 +1,10 @@
+import axios from "axios";
 import styled from "styled-components";
 import { BsHeart, BsHeartFill } from "react-icons/bs";
 import reactStringReplace from "react-string-replace";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useCallback } from "react";
+import useAuth from "../hooks/useAuth.js";
 
 function parseHashtags(caption) {
   const regex = /(?<=^|\s)(?:[#＃]([a-z0-9]+))(?=$|\s)/gi;
@@ -17,6 +20,39 @@ function parseHashtags(caption) {
 }
 
 export default function PostCard({ post }) {
+  const { auth, setAuth } = useAuth();
+  const navigate = useNavigate();
+  const [like, setLike] = useState(post.liked);
+  const [likePending, setLikePending] = useState(false);
+  const [likesCount, setLikesCount] = useState(post.likesCount);
+
+  const handleLike = useCallback(() => {
+    setLikePending(true);
+
+    const url = `http://localhost:5000/posts/${post.id}/${
+      like ? "unlike" : "like"
+    }`;
+
+    axios
+      .post(url, {}, { headers: { authorization: `Bearer ${auth.token}` } })
+      .then((res) => {
+        setLike(!like);
+        console.log(res.data);
+        setLikesCount(res.data.likesCount);
+      })
+      .catch((err) => {
+        console.error(err);
+        if (err.response.status === 401) {
+          localStorage.clear();
+          setAuth(null);
+          navigate("/");
+        }
+      })
+      .finally(() => {
+        setLikePending(false);
+      });
+  }, [likePending, setLikePending, like, setLike]);
+
   return (
     <Container>
       <ProfileAndLikesContainer>
@@ -26,9 +62,10 @@ export default function PostCard({ post }) {
             alt={`${post.author.name} profile picture`}
           />
         </Link>
-        <button>
-          {post.liked ? <BsHeartFill className="liked" /> : <BsHeart />}
+        <button onClick={handleLike}>
+          {like ? <BsHeartFill className="liked" /> : <BsHeart />}
         </button>
+        <span>{`${likesCount} like${likesCount !== 1 ? "s" : ""}`}</span>
       </ProfileAndLikesContainer>
       <PostContent>
         <Link to={`/user/${post.author.id}`}>
@@ -60,9 +97,9 @@ const ProfileAndLikesContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 20px;
 
   & img {
+    margin-bottom: 20px;
     width: 50px;
     height: 50px;
     object-fit: contain;
@@ -70,6 +107,7 @@ const ProfileAndLikesContainer = styled.div`
   }
 
   & button {
+    margin-bottom: 5px;
     padding: 0;
     border: none;
     background-color: transparent;
@@ -83,6 +121,12 @@ const ProfileAndLikesContainer = styled.div`
 
   & svg.liked {
     fill: #ac0000;
+  }
+
+  & span {
+    font-family: "Lato", sans-serif;
+    font-size: 11px;
+    color: white;
   }
 `;
 
