b584a3cdd097953e5769f11ae1e047db9d57cb90
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-06-07 20:49:00 -0300

    fix: profile page name and picture

--- src/components/PostsPageLayout.js
+++ src/components/PostsPageLayout.js
@@ -2,12 +2,23 @@ import styled from "styled-components";
 import Header from "./Header.js";
 import TrendingHashtags from "./TrendingHashtags.js";
 
-export default function PostsPageLayout({ children, heading }) {
+export default function PostsPageLayout({ children, heading, profilePicture }) {
   return (
     <>
       <Header />
       <PageContainer>
-        <h2>{heading}</h2>
+        <PageHeading>
+          {
+            heading !== "timeline" ? 
+              <><img 
+                src={profilePicture} 
+                alt={heading} 
+              />
+              <h2>{heading}</h2></>
+              : <h2>{heading}</h2>
+          }
+        </PageHeading>
+        
         <PageContent>
           <Container>{children}</Container>
           <div>
@@ -22,14 +33,26 @@ export default function PostsPageLayout({ children, heading }) {
 const PageContainer = styled.div`
   margin: 50px auto;
   width: 1000px;
+`;
+
+const PageHeading = styled.div`
+  display: flex;
+
+  & img {
+    width: 50px;
+    height: 50px;
+    margin: 0px 20px;
+    object-fit: contain;
+    border-radius: 100%;
+  }
 
   & h2 {
+    color: white;
     font-family: "Oswald", sans-serif;
     font-size: 43px;
     font-weight: 700;
-    color: white;
   }
-`;
+`
 
 const PageContent = styled.div`
   display: flex;
